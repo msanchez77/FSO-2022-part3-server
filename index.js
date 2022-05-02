0f704362a6869108676e31251f82d2e06050362d
Matt Sanchez <mattasanchez77@gmail.com>
2022-05-02 16:56:30 -0700

    uibuild

--- index.js
+++ index.js
@@ -50,32 +50,19 @@ app.get('/api/persons/:id', (request, response) => {
 
 
 // POST
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  if (!body.name && !body.number) {
-    let error_str = "name and number is missing"
-    return response.status(400).json({
-      error: error_str
-    })
-  }  
-  
-  if (!body.name || !body.number) {
-    let error_str = 
-      `${body.name ? '' : 'name'}${body.number ? '' : 'number'} is missing`
-    return response.status(400).json({
-      error: error_str
-    })
-  }
 
   const person = new Person({
     name: body.name,
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
+  person.save()
+    .then(savedPerson => {
     response.json(savedPerson)
-  })
+    })
+    .catch(error => next(error))
 })
 
 
@@ -101,7 +88,9 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new:true })
+  Person.findByIdAndUpdate(
+    request.params.id, person, 
+    { new:true, runValidators: true, context:'query' })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
@@ -122,7 +111,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
